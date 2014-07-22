20dc43fb5d292d5c20b06a911a3b8370ea2b6e00
ryanki1 <contactkieran@hotmail.com>
2014-07-22 18:47:59 +0200

    Bout of angular testing from java IDE

---
+++ login2Contact-web/src/main/webapp/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function(){
+    angular.module('alzApp', [
+    /*
+     * TODO: rename security.login to loginModule
+     */
+    'ngSanitize',
+    'ngRoute',
+    'ui.bootstrap',
+    'security.login',
+    'contactModule',
+    'security'
+]);
+
+angular.module('alzApp')
+    .config([
+        '$locationProvider',
+        function( $locationProvider ) {
+            $locationProvider
+            .html5Mode(false)
+            .hashPrefix('');
+        }
+    ]);
+}());
---
+++ login2Contact-web/src/main/webapp/scripts/module/fred/controller/janeController.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function(){
+    angular.module('contactModule')
+        .controller('janeController', ['$scope', function($scope){
+                $scope.x = '100';
+                $scope.y = '100';
+    }]);
+}());
+
